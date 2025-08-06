cb5801bf546dd77a4b7d6d04f594b812c8199567
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-06 12:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let prices = document.querySelectorAll(".price");
+	let sum = 0;
+	prices.forEach((price) => {
+		sum += Number(price.textContent);
+	});
+
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	td.textContent = sum;
+
+	tr.appendChild(td);
+	document.getElementsByTagName("table")[0].appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
